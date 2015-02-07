54c7521e07b01c548a6a733b43f790abf18391c1
Ashwin Raghavachari <ashwin@localhost.localdomain>
2015-02-07 16:14:36 -0500

    [demo] added demo folder and files

---
+++ demo/demo2.py
@@ -0,0 +1,9 @@
+# gets the nth element in the fibonacci sequence
+def fibonacci(n):
+  if n < 0:
+    return -1
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  return fibonacci(n-1)+fibonacci(n-2);
---
+++ demo/testit.py
@@ -0,0 +1,1 @@
+from demo2 import fibonacci
