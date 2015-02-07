14e01b2fd06b95d1e08165df60b3bf1f0cf84d74
Ashwin Raghavachari <ashwin@localhost.localdomain>
2015-02-07 13:12:45 -0500

    [back/database]fixed thing

--- back/database/accounts.py
+++ back/database/accounts.py
@@ -26,63 +26,75 @@ def createDB():
     prob = cur.fetchall()
 
 # add a user's profile to profiles table
-def addProfile(uid, name1, info1, imgpath1):
+def addProfile(uid1, name1, info1, imgpath1):
+  con = lite.connect('accounts.db')
   with con:
     cur = con.cursor()
-    cur.execute('INSERT INTO profiles VALUES("+str(uid)+", "+name1+", "+info1+", "+imgpath1+")')
+    params = (str(uid1), name1, info1, imgpath1)
+    cur.execute("INSERT INTO profiles VALUES(?,?,?,?)", params)
 
 # add problem info to problems table
-def addProblem(uid, sbsl, problem, timestmp):
+def addProblem(uid1, subsol1, probid1, time1):
+  con = lite.connect('accounts.db')
   with con:
     cur = con.cursor()
-    cur.execute('INSERT INTO problems VALUES("+str(uid)+", "+sbsl+", "+str(problem)+", "+str(timestmp)")')
+    params = (str(uid), subsol1, str(pobid1), str(time1));
+    cur.execute("INSERT INTO problems VALUES(?,?,?,?)", params)
 
 # get all profile data for user from profiles table
 def getUser(userid):
+  con = lite.connect('accounts.db')
   with con:
     cur = con.cursor()
-    cur.execute('SELECT * FROM profiles WHERE uid == userid')
+    param = (str(userid))
+    cur.execute('SELECT * FROM profiles WHERE uid = ?', param)
     data = cur.fetchall()
     return data
 
 # get all problem data from the problems table
 def getProbHist(userid):
+  con = lite.connect('accounts.db')
   with con:
     cur = con.cursor()
-    cur.execute('SELECT * FROM problems WHERE uid == userid')
+    param = (str(userid))
+    cur.execute('SELECT * FROM problems WHERE uid = ?', param)
     data = cur.fetchall()
     return data
 
 # put together html contents
 def getHTML(userid):
   profileinfo = getUser(userid)
-  name = profileinfo["name"]
-  info = profileinfo["info"]
-  imgpath = profileinfo["imgpath"]
+  name = (profileinfo[0])[1]
+  info = (profileinfo[0])[2]
+  imgpath = (profileinfo[0])[3]
 
   probinfo = getProbHist(userid)
-  subsol = probinfo["subsol"]
-  probid = probinfo["probid"]
-  timestamp = probinfo["time"]
-  ret = '''<div class = "container">\n
-             <div class = "raw">\n
-               <img src = '+imgpath+'>\n
-               <H3 class = "col">'+name+'</H3>\n
+  subsol = ''
+  probid = ''
+  timestamp = ''
+  if len(probinfo) > 0:
+    subsol = (probinfo[0])[1]
+    probid = (probinfo[0])[2]
+    timestamp = (probinfo[0])[3]
+  ret = '''<div class = "container">
+             <div class = "raw">
+               <img src = '+imgpath+'>
+               <H3 class = "col-md-3 col-md-offset-3">'+name+'</H3>
                <Br>
-               <H4 class = "">'+info+'</H4>\n
+               <H4 class = "col-md-2 col-md-offset-2">'+info+'</H4>
              </div>
-             <div class = "raw">\n
-               <table>\n
-                 <th>\n
-                   <td>'+probid+'</td>\n
-                   <td>'+timstamp+'</td>\n
-                 </th>\n
-                 <tr>\n
-                   <td>'+'____'+'</td>\n
-                   <td>'+'____'+'</td>\n
-                   <td>'+'____'+'</td>\n
-                 </tr>\n
-               </table>\n
-             </div>\n
+             <div class = "raw">
+               <table>
+                 <th>
+                   <td>'+probid+'</td>
+                   <td>'+timstamp+'</td>
+                 </th>
+                 <tr>
+                   <td>'+'____'+'</td>
+                   <td>'+'____'+'</td>
+                   <td>'+'____'+'</td>
+                 </tr>
+               </table>
+             </div>
            </div>'''
   return ret
