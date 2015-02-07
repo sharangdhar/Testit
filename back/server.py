6510ec957a68688949e015b8cc9388e8d6754b5b
Sohil Shah <sohils@localhost.localdomain>
2015-02-06 20:45:39 -0500

    [server] added logging, began work on upload/text areas

--- back/server.py
+++ back/server.py
@@ -4,7 +4,7 @@ import BaseHTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 
 from os import curdir, sep
-import cgi
+import cgi, sys
 
 PORT = '9001'
 
@@ -22,11 +22,16 @@ class handler (BaseHTTPRequestHandler):
                 mime='text/html'
             if self.path.endswith(".js"):
                 mime='application/javascript'
+            if self.path.endswith(".jpg"):
+                mime='image/jpg'
+            if self.path.endswith(".gif"):
+                mime='image/gif'
+            if self.path.endswith(".png"):
+                mime='image/png'
             if self.path.endswith(".css"):
                 mime='text/css'
 
             if mime:
-                #Open the static file requested and send it
                 file = open(curdir + sep + self.path) 
                 self.send_response(200)
                 self.send_header('Content-type',mime)
@@ -51,15 +56,61 @@ class handler (BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.end_headers()
                 self.wfile.write("Thanks %s !" % form["your_name"].value)
-            return			
+            return
+
+    def deal_post_data(self):
+        boundary = self.headers.plisttext.split("=")[1]
+        remainbytes = int(self.headers['content-length'])
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        if not boundary in line:
+            return (False, "Content NOT begin with boundary")
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        fn = re.findall(r'Content-Disposition.*name="file"; filename="(.*)"', line)
+        if not fn:
+            return (False, "Can't find out file name...")
+        path = self.translate_path(self.path)
+        fn = os.path.join(path, fn[0])
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        line = self.rfile.readline()
+        remainbytes -= len(line)
+        try:
+            out = open(fn, 'wb')
+        except IOError:
+            return (False, "Can't create file to write, do you have permission to write?")
+                
+        preline = self.rfile.readline()
+        remainbytes -= len(preline)
+        while remainbytes > 0:
+            line = self.rfile.readline()
+            remainbytes -= len(line)
+            if boundary in line:
+                preline = preline[0:-1]
+                if preline.endswith('\r'):
+                    preline = preline[0:-1]
+                out.write(preline)
+                out.close()
+                return (True, "File '%s' upload success!" % fn)
+            else:
+                out.write(preline)
+                preline = line
+        return (False, "Unexpect Ends of data.")
+
+    def log_message(self, format, *args):
+        log = open(".log", 'a')
+        log.write("%s - - [%s] %s\n" % (self.address_string(), self.log_date_time_string(), format % args)) 
+        log.close()
+        return
 
 
 try:
     server = BaseHTTPServer.HTTPServer(('', 9001), handler)
-    print 'Starting TestIt server on port ' + PORT + '!'
+    print 'Starting TestIt server on port ' + PORT + '!\n'
     server.serve_forever()
 
 except KeyboardInterrupt:
-    print 'Exiting...\n'
+    print 'Exiting...\nThank you for using TestIt server!. We sincerely hope you have a pleasant day :)!\n'
     server.socket.close()
     exit(0)
